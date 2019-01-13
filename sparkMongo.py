8cdc45b500d56464bd28a33b39d19abfc793cfa5
Rohan Raj <20280562+rajrohan@users.noreply.github.com>
2019-01-14 00:08:08 +0100

    Update SparkMongo.py

--- sparkMongo.py
+++ sparkMongo.py
@@ -1,10 +1,4 @@
 from pyspark.sql import SparkSession,SQLContext
-from collections import namedtuple
-from bson.son import SON
-from pyspark import SparkContext
-from pyspark.sql.functions import desc
-from kafka import KafkaConsumer
-from pyspark.streaming import StreamingContext
 
 
 
@@ -19,29 +13,6 @@ sqlContext = SQLContext(my_spark)
 df = my_spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
 df.printSchema()
 df.createOrReplaceTempView("temp")
-some_fruit = sqlContext.sql("SELECT tag, SUM(count) as frequency FROM temp GROUP BY tag ORDER BY frequency desc")
-some_fruit.show(10)
+top10Keywords = sqlContext.sql("SELECT tag, SUM(count) as frequency FROM temp GROUP BY tag ORDER BY frequency desc")
+top10Keywords.show(10)
 
-#df.registerTempTable("tempTable")
-#SQLContext.registerDataFrameAsTable(df, "tempTable")
-#SQLContext.sql()
-#sqlContext = SQLContext(my_spark)
-#result= sqlContext.sql('SELECT tweet,count(*) as frequency FROM tempTable GROUP BY tweet ORDER BY tweet')
-
-
-
-#result = sqlContext.sql('SELECT tag, count from tweets')
-#result.show()
-# df.printSchema()
-# df['tweet']= df['tweet'].str.split(" ", n = 1, expand = True)
-# df.show()
-# df.filter( lambda word: word.lower().startswith("#") ) \
-# .map( lambda word: ( word.lower(), 1 ) ) \
-# .reduceByKey( lambda a, b: a + b ) \
-# .map( lambda rec: Tweet( rec[0], rec[1] ) ) \
-# .foreachRDD( lambda rdd: rdd.toDF().sort( desc("count") ))\
-# .limit(10).registerTempTable("tweets")
-
-#top_10_tweets = SQLContext.sql( 'Select tag, count from tweets' )
-#top_10_df = top_10_tweets.toPandas()
-#top_10_df.head()
